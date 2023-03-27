15e4790ffd845c3ff939ee9b31be9a05e3f15045
Cris-Aian <crisaianvergara@gmail.com>
2023-03-27 14:17:26 +0800

    scan receipt almost done

--- backend/scan_receipt/views.py
+++ backend/scan_receipt/views.py
@@ -3,25 +3,159 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Scan
 from .serializers import ScanSerializer
-from rest_framework.permissions import AllowAny
+from PIL import Image
+from doctr.models import ocr_predictor
+import re
+import nltk
+from .utils import extract_and_format_date, preprocess, classify_description
+from PIL import Image
+from io import BytesIO
+import numpy as np
 
 
 class ScanListAPIView(APIView):
-    permission_classes = (AllowAny,)
-
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         scans = Scan.objects.filter(user=request.user.id)
         serializer = ScanSerializer(scans, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
+        image_data = request.data.get("image")
+        image_bytes = BytesIO(image_data.read())
+        receipt = Image.open(image_bytes)
+
+        # OCR MODEL
+        model = ocr_predictor(
+            det_arch="db_resnet50", reco_arch="crnn_mobilenet_v3_small", pretrained=True
+        )
+
+        # RUN OCR
+        image = [np.array(receipt)]
+
+        result = model(image)
+
+        # OCR RESULT
+        blocks = result.export()["pages"][0]["blocks"]
+        lines = [line["lines"] for line in blocks]
+        values = [word["words"] for line in lines for word in line]
+
+        result = []
+        for lst in values:
+            text = " ".join([item["value"] for item in lst])
+            result.append(text)
+
+        # DATE PATTERNS
+        date_patterns = [
+            r"\d{1,2}/\d{1,2}/\d{2}",
+            r"\s+(\d{1,2}/\d{1,2}/\d{4})",
+            r"Date:\s(\d{4}-\d{2}-\d{2})",
+            r"\d{2}-\d{2}-\d{2}",
+            r"\d{2}/\d{2}/\d{2}",
+            r"\d{2}/\d{2}/\d{4}",
+            r"\b(\d{1,2} [A-Za-z]{3} \d{2})\b",
+            r"\b([A-Z][a-z]{2} \d{1,2},\d{4})\b",
+        ]
+
+        # VENDOR
+        new_result = [item for item in result if len(item) >= 2]
+        vendor = new_result[0].translate(str.maketrans("", "", ".?"))
+
+        # TAX
+        format_amount_and_tax = r"\d+\.\d+"
+
+        tax = None
+
+        for i in range(len(result)):
+            if "Sales Tax 6.25%" in result[i]:
+                tax = re.search(format_amount_and_tax, result[i + 10]).group()
+                break
+            elif "Sales Tax" in result[i]:
+                tax = re.search(format_amount_and_tax, result[i + 2]).group()
+                break
+            elif "VAT Amount(12%)" in result[i]:
+                tax = re.search(format_amount_and_tax, result[i - 6]).group()
+                break
+            elif "SALES TAX AMOUNT" in result[i]:
+                s = result[i - 3]
+                tax = re.search(
+                    format_amount_and_tax,
+                    "${:,.2f}".format(float(s[1:].replace(",", "."))),
+                ).group()
+                break
+            elif "Tax(RM)" in result[i]:
+                tax = re.search(format_amount_and_tax, f"0{result[i + 2]}").group()
+                break
+            elif "TAX 1" in result[i]:
+                tax = re.search(format_amount_and_tax, result[i - 37]).group()
+                break
+            elif "TAX" in result[i]:
+                tax = re.search(format_amount_and_tax, result[i - 1]).group()
+                break
+
+        # AMOUNT AND CURRENCY
+        amount = None
+        currency = None
+
+        currency_symbols = {
+            "$": "USD",
+            "£": "GBP",
+            "€": "EUR",
+            "¥": "JPY",
+            "₩": "KRW",
+            "₱": "PHP",
+            "P": "PHP",
+            "₹": "INR",
+        }
+
+        price_regex = r"([\$£€¥₩₱₹])(\d+(?:\.\d{1,2})?)"
+
+        try:
+            prices = []
+            for item in result:
+                match = re.search(price_regex, item)
+                if match:
+                    symbol = match.group(1)
+                    price = float(match.group(2))
+                    prices.append(price)
+                    if symbol in currency_symbols:
+                        currency = currency_symbols[symbol]
+            amount = max(prices)
+
+        except ValueError:
+            for i in range(len(result)):
+                if "Amount" in result[i] and "Due" in result[i + 4]:
+                    amount = re.search(format_amount_and_tax, result[i + 5]).group()
+                    currency = "PHP"
+                    break
+                elif "Total:" in result[i]:
+                    amount = re.search(format_amount_and_tax, result[i + 4]).group()
+                    break
+                elif "Total Sales Inclusive GST) RM" in result[i]:
+                    amount = re.search(format_amount_and_tax, result[i + 1]).group()
+                    break
+                elif "SUBTOTAL" in result[i] and "TOTAL" in result[i + 4]:
+                    amount = re.search(format_amount_and_tax, result[i - 33]).group()
+                    break
+
+        # DESCRIPTION
+        # DOWNLOAD REQUIRED NLTK DATA
+        nltk.download("punkt", quiet=True)
+        nltk.download("wordnet", quiet=True)
+        nltk.download("stopwords", quiet=True)
+
+        for receipt_text in result:
+            filtered_words = preprocess(receipt_text)
+            description = classify_description(filtered_words)
+
+        formatted_date = extract_and_format_date(result, date_patterns)
+
         data = {
-            "date": request.data.get("date"),
-            "vendor": request.data.get("vendor"),
-            "amount": request.data.get("amount"),
-            "tax": request.data.get("tax"),
-            "currency": request.data.get("currency"),
-            "description": request.data.get("description"),
+            "date": formatted_date,
+            "vendor": vendor,
+            "amount": amount,
+            "tax": tax,
+            "currency": currency,
+            "description": description,
             "user": request.user.id,
         }
 
@@ -33,15 +167,13 @@ class ScanListAPIView(APIView):
 
 
 class ScanDetailApiView(APIView):
-    permission_classes = (AllowAny,)
-
     def get_object(self, scan_id, user_id):
         try:
             return Scan.objects.get(id=scan_id, user_id=user_id)
         except Scan.DoesNotExist:
             return None
 
-    def get(self, request, scan_id, *args, **kwargs):
+    def get(self, request, scan_id):
         scan_instance = self.get_object(scan_id, request.user.id)
         if not scan_instance:
             return Response(
@@ -52,7 +184,7 @@ class ScanDetailApiView(APIView):
         serializer = ScanSerializer(scan_instance)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def put(self, request, scan_id, *args, **kwargs):
+    def put(self, request, scan_id):
         scan_instance = self.get_object(scan_id, request.user.id)
         if not scan_instance:
             return Response(
@@ -76,7 +208,7 @@ class ScanDetailApiView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, scan_id, *args, **kwargs):
+    def delete(self, request, scan_id):
         scan_instance = self.get_object(scan_id, request.user.id)
         if not scan_instance:
             return Response(
--- frontend/pages/scan-receipt/index.js
+++ frontend/pages/scan-receipt/index.js
@@ -1,80 +1,122 @@
 import Head from "next/head";
-import { Table, Button, Form, Upload } from 'antd'
-import { InboxOutlined } from '@ant-design/icons';
+import { useState } from "react";
+import { Table, Button, Form, Upload } from "antd";
+import { InboxOutlined } from "@ant-design/icons";
 
 const { Dragger } = Upload;
 
 export const getStaticProps = async () => {
-  const res = await fetch('http://127.0.0.1:8000/scans/api/');
+  const res = await fetch("http://127.0.0.1:8000/scans/api/");
   const data = await res.json();
 
   return {
-    props: { receipts: data }
-  }
-}
+    props: { receipts: data },
+  };
+};
 
-const ScanReceipt = ({receipts}) => {
-    const columns = [
-        {
-            title: 'Date',
-            dataIndex: 'date',
-            key: 'date'
-        },
-        {
-            title: 'Vendor',
-            dataIndex: 'vendor',
-            key: 'vendor'
-        },
-        {
-            title: 'Amount',
-            dataIndex: 'amount',
-            key: 'amount'
-        },
-        {
-            title: 'Tax',
-            dataIndex: 'tax',
-            key: 'tax'
-        },
-        {
-            title: 'Currency',
-            dataIndex: 'currency',
-            key: 'currency'
-        },
-        {
-            title: 'Description',
-            dataIndex: 'description',
-            key: 'description'
-        }
-    ]
-    
-    return ( 
-        <>
-            <Head>
-                <title>Zia Apps | Scan Receipt</title>
-                <meta name='keywords' content='zia apps'/>
-            </Head>  
-            <div>
-                <h1>Scan Receipt</h1>
-                <Form>
-                    <Form.Item>
-                        <Dragger>
-                            <p className="ant-upload-drag-icon">
-                                <InboxOutlined />
-                            </p>
-                            <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                            <p className="ant-upload-hint">Support for a single upload.</p>
-                        </Dragger>
-                    </Form.Item>
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit">
-                            Scan Receipt
-                        </Button>
-                    </Form.Item>
-                </Form>
-                <Table dataSource={receipts} columns={columns} rowKey="id" />
-            </div>
-        </>
-     );
-}
- 
+const ScanReceipt = ({ receipts }) => {
+  const [file, setFile] = useState(null);
+  const [uploading, setUploading] = useState(false);
+
+  const handleUpload = async () => {
+    if (!file) {
+      console.error("No file selected");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("image", file);
+    setUploading(true);
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/scans/api/", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+      console.log("Server response:", data);
+    } catch (error) {
+      console.error("Error during upload:", error);
+    }
+
+    setUploading(false);
+  };
+
+  const columns = [
+    {
+      title: "Date",
+      dataIndex: "date",
+      key: "date",
+    },
+    {
+      title: "Vendor",
+      dataIndex: "vendor",
+      key: "vendor",
+    },
+    {
+      title: "Amount",
+      dataIndex: "amount",
+      key: "amount",
+    },
+    {
+      title: "Tax",
+      dataIndex: "tax",
+      key: "tax",
+    },
+    {
+      title: "Currency",
+      dataIndex: "currency",
+      key: "currency",
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+    },
+  ];
+
+  return (
+    <>
+      <Head>
+        <title>Zia Apps | Scan Receipt</title>
+        <meta name="keywords" content="zia apps" />
+      </Head>
+      <div>
+        <h1>Scan Receipt</h1>
+        <Form>
+          <Form.Item>
+            <Dragger
+              beforeUpload={(file) => {
+                console.log("File selected:", file);
+                setFile(file);
+                return false;
+              }}
+            >
+              <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p className="ant-upload-text">
+                Click or drag file to this area to upload
+              </p>
+              <p className="ant-upload-hint">Support for a single upload.</p>
+            </Dragger>
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              onClick={handleUpload}
+              disabled={!file || uploading}
+            >
+              Scan Receipt
+            </Button>
+          </Form.Item>
+        </Form>
+        <Table dataSource={receipts} columns={columns} rowKey="id" />
+      </div>
+    </>
+  );
+};
+
 export default ScanReceipt;
